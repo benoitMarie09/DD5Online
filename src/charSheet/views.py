da38460d422cb9250cc4f51d2035dbd1d3543545
bouduben <bouduben@gmail.com>
2022-10-20 15:48:18 +0200

    charSheet app premier pas

--- src/charSheet/views.py
+++ src/charSheet/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 
 def charSheet(request):
-    return render(request, 'charSheet/charSheet.html')
+    r = requests.get(
+        ' https://www.dnd5eapi.co/api/classes', params=request.GET)
+    classes_data = r.json()
+    context = dict()
+    context['classes'] = []
+    for DDclass in classes_data['results']:
+        context['classes'].append(DDclass['index'])
+    return render(request, 'charSheet/charSheet.html', context)
