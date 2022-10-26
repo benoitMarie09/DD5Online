44b1ada946cf0ab30588f5a24de0026395c139a8
bouduben <bouduben@gmail.com>
2022-10-26 11:56:00 +0200

    creation des models race et capaciterace avec json complété

--- src/charSheet/admin.py
+++ src/charSheet/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import PJ, Caracteristique, Classe, Competence
+from .models import PJ, Caracteristique, Classe, Competence, Race, RaceCapacite, BonusCaracteristique, RaceAdmin, CaracteristiqueAdmin
 from .forms import ClasseAdmin, PJAdmin
 # Register your models here.
+
 admin.site.register(PJ, PJAdmin)
 admin.site.register(Competence)
 admin.site.register(Classe, ClasseAdmin)
-admin.site.register(Caracteristique)
+admin.site.register(Caracteristique, CaracteristiqueAdmin)
+admin.site.register(Race, RaceAdmin)
+admin.site.register(RaceCapacite)
+admin.site.register(BonusCaracteristique)
+
--- src/charSheet/models.py
+++ src/charSheet/models.py
@@ -1,21 +1,48 @@
 from django.db import models
+from django.contrib import admin
 
 
 # Create your models here.
 
+class RaceCapacite(models.Model):
+    id = models.CharField(primary_key=True, max_length=50)
+    desc = models.TextField(null=True)
 
-"""class Character(models.Model):
-    name = models.CharField(max_length=100)
-    race = models.CharField(max_length=100)
-    classe = models.CharField(max_length=100,choices())
-    background = models.CharField(max_length=100)
-    # stats
-    strength = models.IntegerField()
-    dexterity = models.IntegerField()
-    constitution = models.IntegerField()
-    intelligence = models.IntegerField()
-    wisdom = models.IntegerField()
-    charisma = models.IntegerField()"""
+    def __str__(self):
+        return self.id
+
+
+class Race(models.Model):
+    id = models.CharField(primary_key=True, max_length=20)
+    desc = models.TextField(null=True, blank=True)
+    taille_max = models.IntegerField(null=True, blank=True)
+    taille_min = models.IntegerField(null=True, blank=True)
+    vitesse = models.DecimalField(
+        decimal_places=1, max_digits=4, null=True, blank=True)
+    bonus_caracteristique = models.ManyToManyField(
+        'Caracteristique', through='BonusCaracteristique')
+    capacite = models.ManyToManyField('RaceCapacite')
+    famille = models.CharField(null=True, blank=True, max_length=20)
+    #sorts = manytomany
+
+    def __str__(self):
+        return self.id
+
+
+class BonusCaracteristique(models.Model):
+    caracteristique = models.ForeignKey(
+        'Caracteristique', related_name='bonus_caract', on_delete=models.SET_NULL, null=True)
+    race = models.ForeignKey(
+        'Race', related_name='bonus_caract', on_delete=models.SET_NULL, null=True)
+    valeur = models.IntegerField(default=1)
+
+
+class ValeurCaracteristique(models.Model):
+    caracteristique = models.ForeignKey(
+        'Caracteristique', related_name='valeur_caract', on_delete=models.SET_NULL, null=True)
+    PJ = models.ForeignKey(
+        'PJ', related_name='valeur_caract', on_delete=models.SET_NULL, null=True)
+    valeur = models.IntegerField(default=10)
 
 
 class Caracteristique(models.Model):
@@ -31,7 +58,7 @@ class Competence(models.Model):
     id = models.CharField(primary_key=True, max_length=30)
     desc = models.TextField(null=True)
     caracteristique = models.ForeignKey(
-        Caracteristique, null=True, related_name='competences', on_delete=models.CASCADE)
+        'Caracteristique', null=True, related_name='competences', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
@@ -41,9 +68,9 @@ class Classe(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
     desc = models.TextField(null=True)
     dv = models.IntegerField(null=True)
-    choix_competences = models.ManyToManyField(Competence)
+    choix_competences = models.ManyToManyField('Competence')
     nb_competences = models.IntegerField(default=2)
-    jets_sauvegarde = models.ManyToManyField(Caracteristique)
+    jets_sauvegarde = models.ManyToManyField('Caracteristique')
 
     def __str__(self):
         return self.id
@@ -52,8 +79,23 @@ class Classe(models.Model):
 class PJ(models.Model):
     nom = models.CharField(null=True, max_length=100)
     classe = models.ForeignKey(
-        Classe, null=True, related_name='PJs', on_delete=models.CASCADE)
-    maitrise_competences = models.ManyToManyField(Competence)
+        'Classe', null=True, related_name='PJs', on_delete=models.CASCADE)
+    maitrise_competences = models.ManyToManyField('Competence')
 
     def __str__(self):
         return '{}-{}-{}'.format(self.id, self.nom, self.classe)
+
+
+# Gestion des formulaire admin pour le m2m
+
+class BonusCaracteristique_inline(admin.TabularInline):
+    model = BonusCaracteristique
+    extra = 1
+
+
+class RaceAdmin(admin.ModelAdmin):
+    inlines = (BonusCaracteristique_inline,)
+
+
+class CaracteristiqueAdmin(admin.ModelAdmin):
+    inlines = (BonusCaracteristique_inline,)
